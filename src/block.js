a8112bfb0d77ca380d87b3611298f7be32955563
Terje Karlsen <terjeofnorway@gmail.com>
2018-03-20 22:29:18 +0100

    Removed difficulty parameters.

--- src/block.js
+++ src/block.js
@@ -1,43 +1,20 @@
 import sha1 from 'sha1';
 
 class Block {
-  constructor(previousHash = '', content = {}, position = 0, difficulty = ''){
-    this.difficulty = difficulty;
+  constructor(previousHash = '', content = {}, position = 0){
     this.creationDate = Date.now();
     this.previousHash = previousHash;
     this.content = content;
     this.position = position;
     this.hash = null;
-    this.nonce = this.mineNonce();
   }
 
   getHash() {
     return this.hash;
   }
 
-  hashMeetsDifficulty(hash) {
-    return hash.substring(0, this.difficulty.length) === this.difficulty;
-  }
-
-  mineNonce(){
-    let hash = '';
-    let nonce = 0;
-
-    while (!this.hashMeetsDifficulty(hash)) {
-      nonce += 1;
-      hash = this.calculateHash(nonce);
-    };
-
-    this.hash = hash;
-    return nonce;
-  }
-
   calculateHash(nonce = this.nonce) {
-    return sha1(`${nonce}` + this.creationDate + this.previousHash + JSON.stringify(this.content));
-  }
-
-  isBlockValid() {
-    return this.calculateHash(this.nonce) === this.hash;
+    return sha1(this.creationDate + this.previousHash + JSON.stringify(this.content));
   }
 
   getMeta() {
@@ -46,7 +23,6 @@ class Block {
       position: this.position,
       previousHash: this.previousHash,
       hash: this.hash,
-      nonce: this.nonce,
     }
   }
 
