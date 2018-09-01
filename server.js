5c3468575d71b3f8c06114fb18124c23b5faa5dc
Terje Karlsen <terjeofnorway@gmail.com>
2018-09-01 13:40:05 +0200

    Fixed space error in server.js.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 
-const publicDir = `${__dirname} /dist/`;
+const publicDir = `${__dirname}/dist`;
+const index = `${publicDir}/index.html`;
 
 const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'dist')))
-  .get('/', (req, res) => res.sendFile(`${publicDir} /index.html`))
+  .get('/', (req, res) => res.sendFile(index))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
