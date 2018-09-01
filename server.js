bbddd0164f938252cac6ea55e4b4662fd15e4c94
Terje Karlsen <terjeofnorway@gmail.com>
2018-09-01 12:15:26 +0200

    Refactor server script for running node on Heroku.

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ var public = __dirname + "/dist/";
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(public + 'index.html'));
-});
-
-app.use('/', express.static(public));
-
-app.listen(PORT);
+express()
+.use(express.static(path.join(__dirname, 'dist')))
+.get('/', (req, res) => res.sendFile(public + 'index.html'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
