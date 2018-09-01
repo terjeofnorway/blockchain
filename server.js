2b55ab9ac5ab20e902da285dcf458e86542cc059
Terje Karlsen <terjeofnorway@gmail.com>
2018-09-01 11:58:31 +0200

    Fixed port for Heroku deploy

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var path = require('path');
 var app = express();
 var public = __dirname + "/dist/";
 
+const PORT = process.env.PORT || 5000
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(public + 'index.html'));
 });
 
 app.use('/', express.static(public));
 
-app.listen(3000)
+app.listen(PORT);
