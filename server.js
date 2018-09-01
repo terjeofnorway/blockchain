8912f2d259fee21f607c4725c9af036b73f7ad41
Terje Karlsen <terjeofnorway@gmail.com>
2018-09-01 13:20:40 +0200

    Linted server script. Added app.json for Heroku.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const publicDir = __dirname + "/dist/";
 
-const PORT = process.env.PORT || 5000
+const publicDir = `${__dirname} /dist/`;
+
+const PORT = process.env.PORT || 5000;
 
 express()
-.use(express.static(path.join(__dirname, 'dist')))
-.get('/', (req, res) => res.sendFile(publicDir + 'index.html'))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, 'dist')))
+  .get('/', (req, res) => res.sendFile(`${publicDir} /index.html`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
