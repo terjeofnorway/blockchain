f67cb8983dd334fe6ed3f76e831fd732a7359983
Terje Karlsen <terjeofnorway@gmail.com>
2018-09-01 14:06:57 +0200

    Set root path

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(publicDir))
-  .get('/', (req, res) => res.sendFile('index.html'))
+  .get('/', (req, res) => res.sendFile('/index.html'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
